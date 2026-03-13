eabe60d0a6f4d65da44cc51fe881989115970660
abdur4code <abdurrahim0101001@gmail.com>
2026-03-13 15:07:06 +0530

    Add sentence maker and trivia bot implementations for JavaScript practice

---
+++ javascript/javascript-basic/sentenceMaker.js
@@ -0,0 +1,28 @@
+//========FreeCodeCamp========
+// Build a Sentence Maker
+// In this lab, you will create two different stories using a sentence template. You will use variables to store different parts of the story and then output the stories to the console.
+
+let adjective = "giant";
+let noun = "dragon";
+let verb = "angry";
+let place = "volcano";
+let adjective2 = "smoky";
+let noun2 = "marshmallows";
+
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+
+adjective = "tiny";
+noun = "hamster";
+verb = "excited";
+place = "shoe box";
+adjective2 = "glittery";
+noun2 = "sunflower seeds";
+
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
---
+++ javascript/javascript-basic/triviaBot.js
@@ -0,0 +1,22 @@
+//========FreeCodeCamp========
+// Build a JavaScript Trivia Bot
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Byte";
+const botLocation = "the Cloud";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage +  " was famously created in just 10 days by Brendan Eich in 1995.";
+console.log(codingFact);
+
+codingFact = "Despite the name, " + favoriteLanguage + " is not related to Java; it was named that way for marketing reasons.";
+console.log(codingFact);
+
+codingFact = "Initially, " + favoriteLanguage + " was going to be called Mocha, then LiveScript, before finally landing on its current name.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
